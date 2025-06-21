0f4cecb868b24a15af849a4654dfc8b9bc7ec303
Fropok <fropike@gmail.com>
2025-06-22 02:45:48 +0300

    add funcs: solve_quiz_and_get_code and parse_basket_mini_total

--- pages/base_page.py
+++ pages/base_page.py
@@ -17,3 +17,26 @@ class BasePage:
         except NoSuchElementException:
             return False
         return True
+
+    def solve_quiz_and_get_code(self):
+        alert = self.browser.switch_to.alert
+        x = alert.text.split(" ")[2]
+        answer = str(math.log(abs((12 * math.sin(float(x))))))
+        alert.send_keys(answer)
+        alert.accept()
+        try:
+            alert = self.browser.switch_to.alert
+            alert_text = alert.text
+            print(f"Your code: {alert_text}")
+            alert.accept()
+        except NoAlertPresentException:
+            print("No second alert presented")
+
+    def parse_basket_mini_total(self, text):
+        '''Извлекает сумму из строки мини корзины'''
+        try:
+            line_with_value = text.splitlines()[0]
+            value = line_with_value.split(':', 1)[1].strip()
+            return value
+        except (IndexError, ValueError):
+            return ''
