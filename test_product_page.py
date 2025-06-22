38a9540359c1923a5aeee18bc5558bad82c8bdb1
Fropok <fropike@gmail.com>
2025-06-22 22:18:40 +0300

    add skip

--- test_product_page.py
+++ test_product_page.py
@@ -16,7 +16,7 @@ from .pages.product_page import ProductPage
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
 def test_guest_can_add_product_to_basket(browser, link):
-    page = ProductPage(browser, link, 1)
+    page = ProductPage(browser, link)
     page.open()
     page.add_to_basket()
     page.solve_quiz_and_get_code()
@@ -25,6 +25,7 @@ def test_guest_can_add_product_to_basket(browser, link):
     page.assert_product_price_in_basket_mini()
 
 
+@pytest.mark.skip
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
     page = ProductPage(browser, link, 0)
@@ -40,6 +41,7 @@ def test_guest_cant_see_success_message(browser):
     page.should_not_be_success_message()
 
 
+@pytest.mark.skip
 def test_message_disappeared_after_adding_product_to_basket(browser):
     link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
     page = ProductPage(browser, link, 0)
