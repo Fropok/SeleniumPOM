11afb6fbe6ee60b816f1e39a2de239f8fde270ad
Fropok <fropike@gmail.com>
2025-06-22 22:15:49 +0300

    add tests

--- test_product_page.py
+++ test_product_page.py
@@ -16,10 +16,33 @@ from .pages.product_page import ProductPage
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
 def test_guest_can_add_product_to_basket(browser, link):
-    page = ProductPage(browser, link)
+    page = ProductPage(browser, link, 1)
     page.open()
     page.add_to_basket()
     page.solve_quiz_and_get_code()
     page.should_be_success_message()
     page.assert_product_name_in_success_message()
-    page.assert_product_price()
+    page.assert_product_price_in_basket_mini()
+
+
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+    page = ProductPage(browser, link, 0)
+    page.open()
+    page.add_to_basket()
+    page.should_not_be_success_message()
+
+
+def test_guest_cant_see_success_message(browser):
+    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+    page = ProductPage(browser, link, 0)
+    page.open()
+    page.should_not_be_success_message()
+
+
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+    page = ProductPage(browser, link, 0)
+    page.open()
+    page.add_to_basket()
+    page.should_be_disappear_success_message()
