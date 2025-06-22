f54515b8e7e799de226b612193f7ebbfbcc8c331
Fropok <fropike@gmail.com>
2025-06-22 22:14:51 +0300

    add methods: is_not_element_present and is_disappeared

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,6 +1,8 @@
 import math
 
-from selenium.common.exceptions import NoSuchElementException, NoAlertPresentException
+from selenium.common.exceptions import NoSuchElementException, NoAlertPresentException, TimeoutException
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
 
 
 class BasePage:
@@ -11,17 +13,36 @@ class BasePage:
         self.browser.implicitly_wait(timeout)
 
     def open(self):
+        '''Открываем браузер'''
         self.browser.get(self.url)
 
     def is_element_present(self, how, what):
-        '''Проверка элемента на странице'''
+        '''Проверка присутствия элемента на странице'''
         try:
             self.browser.find_element(how, what)
         except NoSuchElementException:
             return False
         return True
 
+    def is_not_element_present(self, how, what, timeout=4):
+        '''Проверка отсутствия элемента на странице'''
+        try:
+            WebDriverWait(self.browser, timeout).until(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return True
+        return False
+
+    def is_disappeared(self, how, what, timeout=4):
+        '''Проверка, что элемент исчез со страницы в течение timeout секунд'''
+        try:
+            WebDriverWait(self.browser, timeout, poll_frequency=1, ignored_exceptions=[TimeoutException]) \
+                .until_not(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return False
+        return True
+
     def solve_quiz_and_get_code(self):
+        '''Решить викторину и получить код для успешного добавления в корзину'''
         alert = self.browser.switch_to.alert
         x = alert.text.split(" ")[2]
         answer = str(math.log(abs((12 * math.sin(float(x))))))
