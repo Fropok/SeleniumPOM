da3e335f8cdfb93297a6a47f79a6cc0b1fdfbc15
Fropok <fropike@gmail.com>
2025-06-23 23:51:41 +0300

    add methods register user

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,5 +1,6 @@
 from .base_page import BasePage
 from .locators import LoginPageLocators
+from faker import Faker
 
 
 class LoginPage(BasePage):
@@ -20,3 +21,24 @@ class LoginPage(BasePage):
     def should_be_register_form(self):
         '''Проверка наличия формы регистрации на странице'''
         assert self.is_element_present(*LoginPageLocators.REGISTRATION_FORM), 'ОШИБКА! Форма регистрации отсутствует'
+
+    def register_new_user(self, email, password):
+        '''Регистрирует нового пользователя'''
+        self.browser.find_element(*LoginPageLocators.REGISTRATION_EMAIL).send_keys(email)
+        self.browser.find_element(*LoginPageLocators.REGISTRATION_PASSWORD_1).send_keys(password)
+        self.browser.find_element(*LoginPageLocators.REGISTRATION_PASSWORD_2).send_keys(password)
+        self.browser.find_element(*LoginPageLocators.REGISTRATION_BUTTON).click()
+
+    @staticmethod
+    def generate_fake_email():
+        '''Генерирует email'''
+        fake = Faker('ru_RU')
+        email = fake.email()
+        return email
+
+    @staticmethod
+    def generate_fake_password():
+        '''Генерирует пароль'''
+        fake = Faker('ru_RU')
+        password = fake.password()
+        return password
